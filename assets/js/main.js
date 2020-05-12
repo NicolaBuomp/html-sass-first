312bba584d912c82f829ee5bd0f94279b203cd17
Nicola <niconix.dev@gmail.com>
2020-05-12 17:11:30 +0200

    Welcome SASS

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	/************************************************************************/
+(() => {
+/*!*********************!*\
+  !*** ./src/main.js ***!
+  \*********************/
+/*! unknown exports (runtime-defined) */
+/*! exports [maybe provided (runtime-defined)] [unused] */
+/*! runtime requirements:  */
+
+})();
+
+(() => {
+/*!************************!*\
+  !*** ./src/style.scss ***!
+  \************************/
+/*! unknown exports (runtime-defined) */
+/*! exports [maybe provided (runtime-defined)] [unused] */
+/*! runtime requirements:  */
+// extracted by mini-css-extract-plugin
+})();
+
+/******/ })()
+;
